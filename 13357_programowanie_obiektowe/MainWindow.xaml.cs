d4c35b67fd92a2c62a079d64a179ebd31b4621b9
murasame112 <79984587+murasame112@users.noreply.github.com>
2022-06-03 13:04:17 +0200

    biblioteki i poczatek todo

--- 13357_programowanie_obiektowe/MainWindow.xaml.cs
+++ 13357_programowanie_obiektowe/MainWindow.xaml.cs
@@ -22,6 +22,12 @@ using System.Xml.Linq;
 
 namespace _13357_programowanie_obiektowe
 {
+    // https://powietrze.gios.gov.pl/pjp/content/api
+
+    // TODO:
+    // 1. Zdecydować konkretnie które z danych chcę przedstawiać i zapisywać do bazy
+    // 2. Zdecydować w jaki sposób będzie można operować na danych
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
